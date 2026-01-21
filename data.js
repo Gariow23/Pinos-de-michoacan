23e5609a089957f30735f045c314c40c0895c92e
Gariow23 <no32@live.com.mx>
2026-01-21 17:14:17 -0600

    Add files via upload

--- data.js
+++ data.js
@@ -1,20 +1,4 @@
 const speciesData=[
-{
- id:1,
- scientific_name:"Pinus pseudostrobus",
- marker:"ITS",
- image:"https://upload.wikimedia.org/wikipedia/commons/8/82/Pinus_pseudostrobus.jpg",
- locations:[
-   {locality:"Uruapan",lat:19.42,lon:-102.063,altitude:1800}
- ]
-},
-{
- id:2,
- scientific_name:"Pinus montezumae",
- marker:"ITS2",
- image:"https://upload.wikimedia.org/wikipedia/commons/4/49/Pinus_montezumae.jpg",
- locations:[
-   {locality:"Zitácuaro",lat:19.436,lon:-100.358,altitude:2400}
- ]
-}
+{ id:1, scientific_name:"Pinus pseudostrobus", marker:"ITS", locations:[{locality:"Uruapan",lat:19.42,lon:-102.063,altitude:1800}]},
+{ id:2, scientific_name:"Pinus montezumae", marker:"ITS2", locations:[{locality:"Zitácuaro",lat:19.436,lon:-100.358,altitude:2400}]}
 ];
--- script.js
+++ script.js
@@ -1,36 +1,13 @@
 const map=L.map('map').setView([19.566,-101.706],7);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
- attribution:'© OpenStreetMap'
-}).addTo(map);
-
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution:'© OpenStreetMap'}).addTo(map);
 let markers=[];
 const select=document.getElementById('speciesSelect');
-
-speciesData.forEach(s=>{
- let o=document.createElement('option');
- o.value=s.id;
- o.textContent=s.scientific_name;
- select.appendChild(o);
-});
-
+speciesData.forEach(s=>{let o=document.createElement('option');o.value=s.id;o.textContent=s.scientific_name;select.appendChild(o);});
 select.addEventListener('change',()=>{
- markers.forEach(m=>map.removeLayer(m));
- markers=[];
-
- const s=speciesData.find(x=>x.id==select.value);
- if(!s)return;
-
- s.locations.forEach(l=>{
-  const popup=`
-   <strong><em>${s.scientific_name}</em></strong><br>
-   Marcador: ${s.marker}<br>
-   Localidad: ${l.locality}<br>
-   Altitud: ${l.altitude} m<br><br>
-   <img src="${s.image}" width="180">
-  `;
-  markers.push(
-   L.marker([l.lat,l.lon]).addTo(map).bindPopup(popup)
-  );
- });
+markers.forEach(m=>map.removeLayer(m));markers=[];
+const s=speciesData.find(x=>x.id==select.value);
+if(!s)return;
+s.locations.forEach(l=>{
+markers.push(L.marker([l.lat,l.lon]).addTo(map).bindPopup(`<strong>${s.scientific_name}</strong><br>Marcador: ${s.marker}<br>${l.locality}<br>${l.altitude} m`));
+});
 });

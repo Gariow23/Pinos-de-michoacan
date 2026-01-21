32a0c10930cff4808e6c00fc9868f73c3b017d10
Gariow23 <no32@live.com.mx>
2026-01-21 17:12:02 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+const speciesData=[
+{
+ id:1,
+ scientific_name:"Pinus pseudostrobus",
+ marker:"ITS",
+ image:"https://upload.wikimedia.org/wikipedia/commons/8/82/Pinus_pseudostrobus.jpg",
+ locations:[
+   {locality:"Uruapan",lat:19.42,lon:-102.063,altitude:1800}
+ ]
+},
+{
+ id:2,
+ scientific_name:"Pinus montezumae",
+ marker:"ITS2",
+ image:"https://upload.wikimedia.org/wikipedia/commons/4/49/Pinus_montezumae.jpg",
+ locations:[
+   {locality:"Zitácuaro",lat:19.436,lon:-100.358,altitude:2400}
+ ]
+}
+];
